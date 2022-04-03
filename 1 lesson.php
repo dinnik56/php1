31e2a76902c372736737bcbd46016f3921475688
dinnik56 <83467441+dinnik56@users.noreply.github.com>
2022-04-03 22:34:22 +0500

    test work1111231

--- 1 lesson.php
+++ 1 lesson.php
@@ -5,4 +5,6 @@
 <h1>hello</h1>
 <h1>hello</h1>ello</h1>
 <h1>hello</h1>ello</h1>
-<h1>hello</h1>
+<h1>hello</h1>git config --global --unse>hello</h1>ello</h1>
+<h1>hello</h1>ello</h1>
+<h1>hello</h1>git config --global --unset http.proxyt http.proxy
